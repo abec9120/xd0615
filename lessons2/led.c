12a2997ff9886dd5fc0ef35654ab1cd988bd2b8b
abec9120 <841733642@qq.com>
2020-06-18 19:31:37 +0800

    第二次作业 余松炎 19140200080

--- lessons2/led.c
+++ lessons2/led.c
@@ -1,12 +1,12 @@
 void setup()
 {
-  pinMode(9, OUTPUT);
+  pinMode(13, OUTPUT);
 }
 
 void loop()
 {
-  digitalWrite(9, HIGH);
+  digitalWrite(13, HIGH);
   delay(1000); // Wait for 1000 millisecond(s)
-  digitalWrite(9, LOW);
+  digitalWrite(13, LOW);
   delay(1000); // Wait for 1000 millisecond(s)
 }
