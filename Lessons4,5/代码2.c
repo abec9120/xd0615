ecf80f004b07f5162ddb6484ff99676de8c7328d
abec9120 <841733642@qq.com>
2020-06-21 12:30:59 +0800

    第四，五次作业 余松炎19140200080

--- Lessons4,5/代码2.c
+++ Lessons4,5/代码2.c
@@ -14,49 +14,25 @@ void setup()
   digitalWrite(7,HIGH);
   digitalWrite(8,LOW);
   digitalWrite(9,LOW);
-  digitalWrite(11,LOW);
   digitalWrite(10,LOW);
+  digitalWrite(11,LOW);
   Serial.begin(9600);
 }
-byte income1,income2,income3,income4;
+byte income[4];
 void loop()
 {
   if(Serial.available()>0)
   {
-  income1=Serial.read();
-  income1=income1-'0';
-  digitalWrite(5,income1&0x1);
-  digitalWrite(2,(income1>>1)&0x1);
-  digitalWrite(3,(income1>>2)&0x1);
-  digitalWrite(4,(income1>>3)&0x1);
+  for(int i=0;i<4;i++)
+  {
+  income[i]=Serial.read();
+  income[i]=income[i]-'0';
+  digitalWrite(5,income[i]&0x1);
+  digitalWrite(2,(income[i]>>1)&0x1);
+  digitalWrite(3,(income[i]>>2)&0x1);
+  digitalWrite(4,(income[i]>>3)&0x1);
   delay(10);
-  digitalWrite(8,HIGH);
-  
-  income2=Serial.read();
-  income2=income2-'0';
-  digitalWrite(5,income2&0x1);
-  digitalWrite(2,(income2>>1)&0x1);
-  digitalWrite(3,(income2>>2)&0x1);
-  digitalWrite(4,(income2>>3)&0x1);
-  delay(10);
-  digitalWrite(9,HIGH);
-  
-  income3=Serial.read();
-  income3=income3-'0';
-  digitalWrite(5,income3&0x1);
-  digitalWrite(2,(income3>>1)&0x1);
-  digitalWrite(3,(income3>>2)&0x1);
-  digitalWrite(4,(income3>>3)&0x1);
-  delay(10);
-  digitalWrite(10,HIGH);
-  
-  income4=Serial.read();
-  income4=income4-'0';
-  digitalWrite(5,income4&0x1);
-  digitalWrite(2,(income4>>1)&0x1);
-  digitalWrite(3,(income4>>2)&0x1);
-  digitalWrite(4,(income4>>3)&0x1);
-  delay(10);
-  digitalWrite(11,HIGH);
+  digitalWrite(i+8,HIGH);
+  }
   }
 }
